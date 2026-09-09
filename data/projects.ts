4f0f60491450f6c4277789b60917337f584291b9
darinkacristi <darinkacristi.work@gmail.com>
2026-09-09 15:56:51 -0300

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -89,7 +89,7 @@ export const projects: Project[] = [
       { member: "darinka", role: "Fotografía" },
     ],
     format: "vertical",
-    cover: "/work/fotos-terramore/terramore1.jpg",
+    cover: "/work/fotos-terramore/terramore1.jpeg",
     images: [
       "/work/fotos-terramore/terramore1.jpeg",
       "/work/fotos-terramore/terramore2.jpeg",
